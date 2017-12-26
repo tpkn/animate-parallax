6586ed5623019b12de3808cf35ee6ac01a65dc07
tpkn <tpkn@users.noreply.github.com>
2017-12-26 21:35:17 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,63 @@
+# Animate Parallax
+
+
+Simple parallax for Adobe Animate
+
+![preview](https://github.com/tpkn/animate-parallax/raw/master/preview.gif)
+
+
+
+### Usage (code for Animate)
+```javascript
+var config = {
+   canvas: document.getElementById('canvas'),
+   center: {x: 100, y: 100},
+   target: this.target,
+   fps: 60,
+   mouse: {x_axis: true, reset_pos: true, easing: 0.018},
+   layers: [
+      {mc: this.layer0, width: 926, height: 330, offset: 50, scale: 1,   easing: 0.2}, 
+      {mc: this.layer1, width: 926, height: 330, offset: 60, scale: 1.1, easing: 0.2}, 
+      {mc: this.layer2, width: 926, height: 330, offset: 70, scale: 1.2, easing: 0.2}, 
+      {mc: this.layer3, width: 926, height: 330, offset: 80, scale: 1.3, easing: 0.2}
+   ]
+}
+
+window.apx = new AnimateParallax(config);
+```
+
+Each layer still could be scaled by both axis:
+```javascript
+layers: [
+   {mc: this.layer0, scale_x: 1, scale_y: 0.5}, 
+]
+```
+
+
+### Config
+| Variable | Type | Description |
+|-------------|:-------------:|-------------|
+| canvas | DOM element | If not set, parallax will not react on mouse movement |
+| target | Object | If set, `mouseX` and `mouseY` would be replaced by `x/y-axis` from this object |
+| center | Object | Parallax center `{x,y}` point. If `false`, then using half of stage width and height |
+| autostart | Boolean | Autostart parallax action, `true` by default |
+| fps | Number | Refresh rate. By default = canvas fps |
+| mouse | Object | `x_axis`: enable mouse x-axis<br />`reset_pos`: when mouse leave the canvas object, reset parallax position to `center_x` and `center_y`<br />`easing`: overrides layer easing |
+
+### Layer settings
+| Variable | Type | Description |
+|-------------|:-------------:|-------------|
+| mc | Object | Createjs object instance name |
+| width | Number | Layer width. If not set, `x-axis` would be ignored |
+| height | Number | Layer height. If not set, `y-axis` would be ignored |
+| offset | Number | Layer's max travel distance in both directions from parallax center |
+| scale | Number | Scale layer programmaticaly if you don't want to do that in Animate |
+| scale_x | Number | Overrides `scale` property |
+| scale_y | Number | Overrides `scale` property |
+| easing | Number | Smaller value = slower layer speed |
+
+### Public methods
+| Method | Type | Description |
+|-------------|:-------------:|-------------|
+| start | Function | - |
+| stop | Function | - |
---
+++ animate-parallax.js
@@ -0,0 +1,125 @@
+/*!
+ * Animate Parallax (v2.0.0.20171224), http://tpkn.me/
+ */
+
+function AnimateParallax(config){
+   if(!createjs){
+      return console.log('(parallax) No CreateJs!');
+   }
+   if(typeof config !== 'object'){
+      return console.log('(parallax) No config!');
+   }
+   if(typeof config === 'object' && !config.layers){
+      return console.log('(parallax) No layers!');
+   }
+   if(config.layers && !config.layers.length){
+      return console.log('(parallax) Empty layers!');
+   }
+
+
+   var canvas = config.canvas;
+   var target = config.target;
+   var layers = config.layers;
+   var autostart = typeof config.autostart !== 'boolean' ? true : config.autostart;
+
+   var fps = 1000 / (isNumber(config.fps) ? config.fps : lib.properties.fps);
+
+   var uid;
+   var i, layer, len = layers.length;
+   var px, py, offset_x, offset_y, easing = 1;
+   var center_x = config.center && isNumber(config.center.x) ? config.center.x : lib.properties.width / 2;
+   var center_y = config.center && isNumber(config.center.y) ? config.center.y : lib.properties.height / 2;
+
+   var mouse = config.mouse;
+   var mouse_x = center_x, mouse_y = center_y, mouse_over = false;
+   var mouse_easing = mouse && mouse.easing ? mouse.easing : 1;
+   var mouse_enabled = config.mouse && (config.mouse.x_axis || config.mouse.y_axis) ? true : false;
+
+
+   for(i = 0; i < len; i++){
+      layer = layers[i];
+
+      // Set layers scale
+      if(layer.scale && isNumber(layer.scale))     layer.mc.scaleX = layer.mc.scaleY = layer.scale;
+      if(layer.scale_x && isNumber(layer.scale_x)) layer.mc.scaleX = layer.scale_x;
+      if(layer.scale_y && isNumber(layer.scale_y)) layer.mc.scaleY = layer.scale_y;
+   }
+
+   if(typeof canvas === 'object' && mouse_enabled){
+      canvas.addEventListener('mouseenter', mouseOver);
+      canvas.addEventListener('mouseleave', mouseOut);
+   }
+
+   if(autostart){
+      startParallax();
+   }
+
+
+   function isNumber(n){
+      return !isNaN(parseFloat(n)) && isFinite(n);
+   }
+   
+   function updateLayers(){
+      for(i = 0; i < len; i++){
+         layer = layers[i];
+
+         if(mouse_over){
+            easing = isNumber(mouse_easing) ? mouse_easing : layer.easing;
+            mouse_x = mouse.x_axis ? stage.mouseX : center_x;
+            mouse_y = mouse.y_axis ? stage.mouseY : center_y;
+         }else{
+            if(target){
+               if(isNumber(mouse_easing)) easing = layer.easing;
+               if(isNumber(target.x)) mouse_x = target.x;
+               if(isNumber(target.y)) mouse_y = target.y;
+            }else{
+               if(mouse.reset_pos){
+                  mouse_x = center_x;
+                  mouse_y = center_y;
+               }
+            }
+         }
+
+         // Calculate offset position if width/height is set
+         // If not, ignore axis
+         if(isNumber(layer.width) && layer.width > 0){
+            offset_x = center_x + (center_x - mouse_x) * layer.offset / center_x;
+         }else{
+            offset_x = layer.mc.x;
+         }
+
+         if(isNumber(layer.height) && layer.height > 0){
+            offset_y = center_y + (center_y - mouse_y) * layer.offset / center_y;
+         }else{
+            offset_y = layer.mc.y;
+         }
+
+         easing = easing <= 0 ? 1 : easing;
+         layer.mc.x += (offset_x - layer.mc.x) * easing;
+         layer.mc.y += (offset_y - layer.mc.y) * easing;
+      }
+   }
+
+   function startParallax(){
+      clearInterval(uid);
+      uid = setInterval(updateLayers, fps);
+   }
+
+   function stopParallax(){
+      clearInterval(uid);
+   }
+
+   function mouseOver(){
+      mouse_over = true;
+   }
+
+   function mouseOut(){
+      mouse_over = false;
+   }
+
+
+   return {
+      start: startParallax, 
+      stop: stopParallax
+   };
+}
